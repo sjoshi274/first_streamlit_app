74e97c6ef5581c3953028ebd4594e3220261c938
shweta joshi <104842267+sjoshi274@users.noreply.github.com>
2022-05-03 13:56:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.text('\U0001F951 Avacado Toast')
 
 streamlit.header('\U0001F34C \U0001F96D  \U0001F352 Build Your Own Fruit Smoothie \U0001F347 \U0001F34A')
 
-streamlit.multiselect("Pick some Fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some Fruits:",list(my_fruit_list.index),['Avacado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
 
