a9f2c6908ebf12190f5ddf764115c53d9792ec44
shweta joshi <104842267+sjoshi274@users.noreply.github.com>
2022-05-03 12:56:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-import pandas  as pd
-my_fruit_list = pd.read_cvs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import pandas
+my_fruit_list = pandas.read_cvs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
 
