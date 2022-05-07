8682754679ba4ba08bdff739c08f31eed76b12ae
shweta joshi <104842267+sjoshi274@users.noreply.github.com>
2022-05-07 10:52:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,21 +53,21 @@ streamlit.header("the fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
- return my_cur.fetchall()
+        return my_cur.fetchall()
 
 # add a u button to load fruit
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(insecure_mode=True   ,  **streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
-streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows)
 
 
 # allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('" +  new_fruit   +"')")
-return "thanks for adding " + new_fruit
+    return "thanks for adding " + new_fruit
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
@@ -75,5 +75,5 @@ if streamlit.button('add a fruit to the list'):
     my_cnx = snowflake.connector.connect(insecure_mode=True   ,  **streamlit.secrets["snowflake"])
     back_from_function  =  insert_row_snowflake(add_my_fruit)
     my_cnx.close()
-streamlit.text(back_from_function)
+    streamlit.text(back_from_function)
 
